bd844c1b20072cb19029beadb29bd6f6e05165a2
satya <kadamsatyajit@gmail.com>
2022-09-05 08:05:48 +0530

    third  commit

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "Congrats ! your application deployed successfully in Azure Platform and ready to deployed on mulitple  !";
 	}
 
 	public static void main(String[] args) {
